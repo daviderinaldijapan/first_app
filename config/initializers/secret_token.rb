ef38656e285e494f6ef4f1e2fd50458aae8bef2d
davide <amigadax@gmail.com>
2012-12-30 12:04:11 +0900

    InitGaz Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '5837970c3c16a341af4712c44788dbce7dc1e081cae7cc746d2047ccd18a94283f86155585cf713f287adcb47f369e36aec36e1ab0f71b983371d7fd01d38a6d'
